049dae406701d7b8186c7c85562fd6d023ceb4ab
Maksim Shesternev <shesternev.m@gmail.com>
2024-09-12 13:31:21 +0300

    Задача 2.1.2 решена, тест пройден

---
+++ src/main/java/Cat.java
@@ -0,0 +1,29 @@
+
+public class Cat {
+    private String name;
+    private byte age;
+
+    public Cat() {
+    }
+
+    public Cat(String name, byte age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public byte getAge() {
+        return age;
+    }
+
+    public void setAge(byte age) {
+        this.age = age;
+    }
+}
